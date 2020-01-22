4e2bee2ee39c4db144486c9ab4c3ad0110f4501b
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2020-01-22 11:19:48 -0300

    Adding authentication

---
+++ src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a href="https://bulma.io" class="navbar-item">
+        <h1 class="has-text-white title">OneBitTinder</h1>
+      </a>
+    </div>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+  nav {
+    background-color: #ff6666;
+  }
+</style>
